a05ed8f8b2bf48795b3acdd3beaf03f21107bca5
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2024-03-11 16:35:10 +0100

    First release

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
+namespace modules {
+    /**
+     * CalliServ Servoboard Servo at Pin C16
+     */
+    //% fixedInstance whenUsed block="Calliserv C16"
+    export const CalliServ0 = new ServoClient("Calliserv0?dev=self&srvo=0&name=C16")
 
+    /**
+      * CalliServ Servoboard Servo at Pin C17
+      */
+    //% fixedInstance whenUsed block="Calliserv C17"
+    export const CalliServ1 = new ServoClient("Calliserv1?dev=self&srvo=1&name=C17")
+
+}
+
+
+namespace servers {
+    function start() {
+        jacdac.productIdentifier = 0x3cc2d4b4
+        jacdac.deviceDescription = "CalliServ 2 servo driver board"
+        jacdac.startSelfServers(() => [
+            new jacdac.ServoServer(AnalogPin.C16, { instanceName: "C16" }),
+            new jacdac.ServoServer(AnalogPin.C17, { instanceName: "C17" }),
+        ])
+    }
+    start()
+}
